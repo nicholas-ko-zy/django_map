6c591318fa4007c7bb2a94614ee88257e4691fc9
nicholas-ko-zy <nicholas-ko-zy@outlook.com>
2025-08-04 18:44:19 +0800

    fixed js modular bug

--- map/static/map/js/map.js
+++ map/static/map/js/map.js
@@ -1,13 +1,10 @@
-// map.js
 import { MAP_CONFIG } from './constants.js';
+// map.js
 // Use Leaflet global L object
 const CENTER = [1.37564, 103.79740];
 const ZOOM_LEVEL = 10.5;
 
 const map = L.map('map').setView(CENTER, ZOOM_LEVEL);
-
-const markerLayer = L.layerGroup().addTo(map);
-window.appMap = { map, markerLayer };
 
 const tileLayer = L.tileLayer(
   `${MAP_CONFIG.TILE_LAYER.URL}?api_key=${MAP_CONFIG.TILE_LAYER.API_KEY}`, 
